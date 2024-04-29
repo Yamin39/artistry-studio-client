4ea81aceb44c5f3eb88c7fa6db57bdbccc3bf42d
Yamin39 <mdmufazzalhossainyamin5010@gmail.com>
2024-04-30 04:18:51 +0600

    added filter functionality in my arts & craft section

--- src/pages/MyArtCraftList/MyArtCraftList.jsx
+++ src/pages/MyArtCraftList/MyArtCraftList.jsx
@@ -23,14 +23,40 @@ const MyArtCraftList = () => {
     getCraftList();
   }, []);
 
+  const handleFilter = (status) => {
+    setLoader(true);
+    fetch(`http://localhost:5000/my-art-&-craft-list/${user.email}/${status}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setMyArtCraftList(data);
+        setLoader(false);
+      });
+  };
+
   return (
     <div className="max-w-[1440px] w-11/12 py-20 mx-auto">
       <div className="text-center">
         <h2 className="font-bold font-serif text-3xl">My Art & Craft List</h2>
       </div>
 
+      <div className="w-fit mx-auto mt-6">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn text-base text-white bg-[#B59460] hover:bg-[#B59460] hover:brightness-90 border-none rounded m-1">
+            Customization
+          </div>
+          <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+            <li>
+              <button onClick={() => handleFilter("Yes")}>Yes</button>
+            </li>
+            <li>
+              <button onClick={() => handleFilter("No")}>No</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+
       {loader ? (
-        <div className="flex flex-col justify-center items-center">
+        <div className="flex flex-col mt-20 justify-center items-center">
           <Lottie className="size-36" animationData={loadingAnimation} />
         </div>
       ) : (
